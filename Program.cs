fc2560d4b5c3be511bdf5edf6ca50f900db96993
michaeljhkim <michaelkim291@gmail.com>
2025-10-31 13:54:23 -0400

    basic working version of callables

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System; 
+
+namespace AsyncTaskRunner {
+
+    public interface ICallable {
+        object? Invoke(params object[] args);
+    }
+
+    public class Callable<T> : ICallable {
+        private readonly Func<object[], T> _func;
+        private readonly int _expectedArgs;
+
+        public Callable(Func<object[], T> func, int expectedArgs = -1) {
+            _func = func;
+            _expectedArgs = expectedArgs;
+        }
+
+        public object Invoke(params object[] args) {
+            if (_expectedArgs >= 0 && args.Length != _expectedArgs)
+                throw new ArgumentException($"Expected {_expectedArgs} arguments, got {args.Length}");
+
+            return _func(args)!;
+        }
+    }
+    
+    class TaskRunner {
+
+        static void Main(string[] args) {
+            var add_test = new Callable<int>(
+                args => {
+                    int a = (int)args[0];
+                    int b = (int)args[1];
+                    return a + b;
+                },
+                expectedArgs: 2
+            );
+
+            var sub_test = new Callable<int>(
+                args => {
+                    int a = (int)args[0];
+                    int b = (int)args[1];
+                    return a - b;
+                },
+                expectedArgs: 2
+            );
+
+            var callables = new List<ICallable> { add_test, sub_test };
+
+            Console.WriteLine(callables[0].Invoke(10, 4));
+            Console.WriteLine(callables[1].Invoke(10, 4));
+        } 
+    } 
+}
