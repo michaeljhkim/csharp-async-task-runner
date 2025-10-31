699a6cc98a03d086990f7733277972465d9fc4b6
michaeljhkim <michaelkim291@gmail.com>
2025-10-31 14:42:08 -0400

    re-organized project to include unit testing

---
+++ async-task-runner.Tests/UnitTest_Callables.cs
@@ -0,0 +1,58 @@
+﻿using Xunit;
+using AsyncTaskRunner;
+
+namespace async_task_runner.Tests;
+
+public class UnitTest_Callables {
+    [Fact]
+    public void add_callables_to_same_container() {
+        // addition function test
+        var add_test = new Callable<int>(
+            args => {
+                int a = (int)args[0];
+                int b = (int)args[1];
+                return a + b;
+            },
+            expectedArgs: 2
+        );
+
+        // subtraction function test
+        var sub_test = new Callable<int>(
+            args => {
+                int a = (int)args[0];
+                int b = (int)args[1];
+                return a - b;
+            },
+            expectedArgs: 2
+        );
+
+        var callables = new List<ICallable>{};
+        // Add them to the list
+        callables.Add(add_test);
+        callables.Add(sub_test);
+
+        // Assert the list contains them
+        Assert.Contains(add_test, callables);
+        Assert.Contains(sub_test, callables);
+
+        // Optional: assert count
+        Assert.Equal(2, callables.Count);
+    }
+
+    [Fact]
+    public void correct_number_of_args_in_callable() {
+        // addition function test
+        var add_test = new Callable<int>(
+            args => {
+                int a = (int)args[0];
+                int b = (int)args[1];
+                return a + b;
+            },
+            expectedArgs: 2
+        );
+
+        // checks if ArgumentException is thrown if arguements count is not equal (as specified with expectedArgs)
+        // 3 args here, 2 args expected     
+        Assert.Throws<ArgumentException>(() => add_test.Invoke(10, 4, "message"));
+    }
+}
---
+++ async-task-runner/Program.cs
@@ -0,0 +1,31 @@
+﻿using System; 
+
+namespace AsyncTaskRunner {
+
+    public interface ICallable {
+        object? Invoke(params object[] args);
+    }
+
+    // T is return type only, arguements can be any type, and can have as many as needed
+    public class Callable<T> : ICallable {
+        private readonly Func<object[], T> _func;
+        private readonly int _expectedArgs;     // makes sure that the number of arguements is consistent
+
+        public Callable(Func<object[], T> func, int expectedArgs = -1) {
+            _func = func;
+            _expectedArgs = expectedArgs;
+        }
+
+        public object Invoke(params object[] args) {
+            if (_expectedArgs >= 0 && args.Length != _expectedArgs) { 
+                throw new ArgumentException($"Expected {_expectedArgs} arguments, got {args.Length}");
+            }
+            return _func(args)!;
+        }
+    }
+    
+    class TaskRunner {
+        static void Main(string[] args) {
+        } 
+    } 
+}
